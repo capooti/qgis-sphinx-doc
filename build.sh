d2654876075f8d31ceca17cb3644bbcbff43bc6a
Paolo Corti <pcorti@gmail.com>
2012-04-12 16:45:50 +0200

    Moved the linfiniti-sphinx-theme module to original one. Other slight modifications.

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ rm -rf _build/
 msgfmt source/translated/it/index.po -o source/translated/it/LC_MESSAGES/index.mo
 msgfmt source/translated/it/introduction.po -o source/translated/it/LC_MESSAGES/introduction.mo
 msgfmt source/translated/it/getting_started.po -o source/translated/it/LC_MESSAGES/getting_started.mo
+msgfmt source/translated/it/features_at_a_glance.po -o source/translated/it/LC_MESSAGES/features_at_a_glance.mo
 # build html
 sphinx-build -b html -a -Dlanguage=en source _build/html/en
 sphinx-build -b html -a -Dlanguage=it source _build/html/it
